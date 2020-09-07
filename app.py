859a3c31fcb53a780072d215d0326efca0f02cce
MuhammadArsalan1996 <52852763+MuhammadArsalan1996@users.noreply.github.com>
2020-09-07 07:15:27 -0700

    Update app.py

--- app.py
+++ app.py
@@ -86,13 +86,12 @@ def predict_api():
     For direct API calls trought request
     '''   
       int_features = request.get_json(force=True)
-      int_features[0] =recommended
-      int_features[1]=recommended1
-      int_features[2]=recommended2
-      int_features[3]=recommended3
-      int_features[4]=recommended4
-  # data = [int_features[0], int_features[1], int_features[2], int_features[3], int_features[4]]
-     
+     # int_features[0] =recommended
+      #int_features[1]=recommended1
+      #int_features[2]=recommended2
+      #int_features[3]=recommended3
+      #int_features[4]=recommended4
+  
     
     final_features = [np.array(int_features)]
     recommended =IMS_model.predict(final_features)
